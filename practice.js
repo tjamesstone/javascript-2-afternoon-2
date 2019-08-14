9e33efdc548bd68fe367e49160e8c38fafe19bba
Tate Stone <tatejamesstone@gmail.com>
2019-08-14 15:22:10 -0600

    Finished problems 1-5

--- practice.js
+++ practice.js
@@ -17,8 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
-
+function first(arr){
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -32,8 +33,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
-
+function last(arr){
+  return( arr[arr.length - 1])
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -47,7 +49,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family) {
+  for(let i = 0; i < family.length; i++){
+  alert(family[i])}
+}
 
 
 
@@ -62,7 +67,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+function reversedLooper(letters) {
+  for(let i = letters.length-1; i >= 0; i--){
+  alert(letters[i])}
+}
+//reversedLooper(letters)
 
 
 
@@ -77,7 +86,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(numbers){
+  let newNums = []
+  for(let i = 0; i < numbers.length; i++){
+    
+    if(numbers[i] %2 === 0){
+      newNums.push(numbers[i])
+    }
+  } return newNums
+}
 
 
 
